7dc6652766898f1ae753e515a92100942a4b73ff
minzzzi <cmj2393a@naver.com>
2024-08-18 19:17:50 +0900

    [난이도 0] Title: 단어의 개수 세기, Time: 0.03 ms, Memory: 30.94 MB, Difficulty: 0 -BaekjoonHub

---
+++ goormlevel/0/47883/1. 단어의 개수 세기/단어의 개수 세기.js
@@ -0,0 +1,12 @@
+// Run by Node.js
+
+const readline = require("readline");
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+rl.on("line", function(line) {
+	console.log(line.split(' ').filter(char => char).length);
+	rl.close();
+});
